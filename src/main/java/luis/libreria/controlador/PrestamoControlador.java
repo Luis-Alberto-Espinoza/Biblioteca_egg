e2d8b1bcb4b5409f2336f9a183c20dd4d62eafbd
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2023-02-25 08:42:29 -0300

    refactorizar prestamo + logica ok

--- src/main/java/luis/libreria/controlador/PrestamoControlador.java
+++ src/main/java/luis/libreria/controlador/PrestamoControlador.java
@@ -22,34 +22,64 @@ public class PrestamoControlador {
     @Autowired
     PrestamoServicio prestamoServicio;
 
-    @GetMapping("/todos")
+    @GetMapping("/tabla")
     public String todos(ModelMap model) {
         List<Prestamo> Prestamos = prestamoServicio.todos();
         model.put("objetoAiterar", Prestamos);
 
+        model.addAttribute("titulo_html", "Lista de Clintes");
         model.addAttribute("formulario", "Tabla de Prestamos");
         model.addAttribute("tituloFormulario", "Gestionar Prestamos");
         model.addAttribute("action", "/prestamo/alta");
+        model.addAttribute("urlDelete", "/prestamo/baja");
+        model.addAttribute("urlEdit", "/prestamo/pre_update");
+        model.addAttribute("urlAdd", "/prestamo/alta");
 
 
-        return "tabla_prestamo";
+        return "prestamo_tabla";
     }
+
+    @GetMapping("/alta")
+    public String alta_cliente(ModelMap model) {
+        model.addAttribute("alta", "alta");
+        model.addAttribute("formulario", "Agrega Prestamo");
+        model.addAttribute("tituloFormulario", "Agregar un nuevo Prestamo");
+        model.addAttribute("action", "/prestamo/save");
+        model.addAttribute("titulo_html", "Lista de Clientes");
+        model.addAttribute("boton", "Agrega un Prestamo");
+        return "prestamo_formulario";
+    }
+
+    @GetMapping("/save")
+    public String save(ModelMap model,
+                       @RequestParam ("prestamo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  LocalDate prestamo,
+                       @RequestParam ("devolucion") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate devolucion,
+                       @RequestParam String id_cliente,
+                       @RequestParam String id_libro
+    ) {
+        prestamoServicio.guardarPrestamo(prestamo,devolucion, id_cliente,id_libro);
+        model.addAttribute("titulo", "genial fue guardado en nuevo Prestamo");
+        return "exito";
+    }
+
 
     @GetMapping("/pre_update")
     public String pre_update(ModelMap model, Long id) {
         Prestamo prestamo = prestamoServicio.buscarXid(id);
         model.put("objetoIterador", prestamo);
+        model.addAttribute("titulo_html", "Lista de Clintes");
+        model.addAttribute("update", "update");
 
         model.addAttribute("formulario", "Formulario Prestamo");
         model.addAttribute("tituloFormulario", "Gestionar Prestamo");
-        model.addAttribute("action", "/prestamo/update_prestamo");
+        model.addAttribute("action", "/prestamo/update");
         model.addAttribute("boton", "Actualizar Prestamo");
 
 
-        return "update_prestamo";
+        return "prestamo_formulario";
     }
 
-    @GetMapping("/update_prestamo")
+    @GetMapping("/update")
     public String actualizarPrestamo(ModelMap model,
                                      @RequestParam long id,
                                      @RequestParam("prestamo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate prestamo,
@@ -73,17 +103,7 @@ public class PrestamoControlador {
         return "exito";
     }
 
-    @GetMapping("/alta")
-    public String alta_Prestamo(ModelMap model,
-                                @RequestParam("prestamo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate prestamo,
-                                @RequestParam("devolucion") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate devolucion,
-                                @RequestParam String id_libro,
-                                @RequestParam String id_cliente
-    ) {
-        prestamoServicio.guardarPrestamo(prestamo, devolucion, id_cliente, id_libro);
-        model.addAttribute("titulo", "genial fue dado de alta al Prestamo");
-        return "exito";
-    }
+
 
     @GetMapping("/baja")
     public String bajaPrestamo(Long id, ModelMap model) {
